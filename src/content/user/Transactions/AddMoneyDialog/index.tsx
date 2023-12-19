6c809b27c6be574e0a1125cb39848cff619d044e
Dhyey Makadia <dj.makadia24@gmail.com>
2023-12-19 23:59:55 +0530

    Add Money Dialog Changes

--- src/content/user/Transactions/AddMoneyDialog/index.tsx
+++ src/content/user/Transactions/AddMoneyDialog/index.tsx
@@ -1,24 +1,16 @@
-import React, { FC, useState } from 'react';
+import React, { FC } from 'react';
 import {
   Dialog,
   DialogTitle,
   DialogContent,
   Grid,
-  FormControl,
-  TextField,
-  Button,
-  DialogActions,
   useTheme,
   useMediaQuery,
-  Slide
+  Slide,
+  IconButton
 } from '@mui/material';
 import { TransitionProps } from '@mui/material/transitions';
-import BidService from 'src/services/bids/index';
-import { toast } from 'react-toastify';
-import { StatusCode } from 'src/utils/constants';
-import WalletService from 'src/services/wallet/index';
-import Swal from 'sweetalert2';
-import environment from 'src/environment';
+import CloseIcon from '@mui/icons-material/Close';
 
 const Transition = React.forwardRef(function Transition(
   props: TransitionProps & {
@@ -37,59 +29,28 @@ type Props = {
 const AddMoneyDialog: FC<Props> = ({ handleClose, isOpen }) => {
   const theme = useTheme();
   const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
-  const [addMoneyAmount, setAddMoneyAmount] = useState<number>(0);
-
-  const handleAddWithdrawRequest = () => {
-    if (addMoneyAmount > 0) {
-      WalletService.UserAddBalance({
-        amount: addMoneyAmount
-      }).then((res) => {
-        if (res.data.success) {
-          toast.success('Withdraw Request Created Successfully');
-          handleClose(true);
-        } else if (
-          res.data.message === 'Your amount cannot be greater than 2000'
-        ) {
-          handleClose();
-          Swal.fire({
-            icon: 'info',
-            title: 'Approval needed',
-            text: 'Kindly submit a request to the administrator to authorize the addition of funds exceeding the amount of ₹2000.'
-          })
-            .then((result) => {
-              if (result.isConfirmed) {
-                const message = encodeURI(
-                  `Hello admin, \n\nI Need to add ${addMoneyAmount} Rupees to my SP Rashi FAL Wallet. \n\nThanks.`
-                );
-                window.open(
-                  `https://wa.me/${environment.contact}?text=${message}`
-                );
-              }
-            })
-            .finally(() => {
-              setAddMoneyAmount(0);
-            });
-        }
-      });
-    } else {
-      toast.error('Withdraw Amount must be greater than zero');
-    }
-  };
-
-  const handleDialogClose = () => {
-    setAddMoneyAmount(0);
-    handleClose();
-  };
   return (
     <Dialog
       fullScreen={fullScreen}
-      onClose={handleDialogClose}
+      onClose={() => handleClose()}
       open={isOpen}
       TransitionComponent={Transition}
       fullWidth
       maxWidth="sm"
     >
       <DialogTitle>Add Money</DialogTitle>
+      <IconButton
+        aria-label="close"
+        onClick={() => handleClose()}
+        sx={{
+          position: 'absolute',
+          right: 8,
+          top: 8,
+          color: (theme) => theme.palette.grey[500]
+        }}
+      >
+        <CloseIcon />
+      </IconButton>
       <DialogContent>
         <Grid container xs={12}>
           <Grid item xs={12}>
